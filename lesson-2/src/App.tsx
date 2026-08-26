0bc1e66addb62876708042e2bc33623bd9e78513
didu4 <solosar42@gmail.com>
2026-08-26 08:19:59 +0400

    создал приложение со списком дел

--- lesson-2/src/App.tsx
+++ lesson-2/src/App.tsx
@@ -1,122 +1,50 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  const tasks = [
+    { id: 1, title: "Купить продукты на неделю...", isDone: false },
+    { id: 2, title: "Полить цветы", isDone: true },
+    { id: 3, title: "Сходить на тренировку", isDone: false },
+    { id: 4, title: "Снизу вверх", isDone: false },
+    { id: 5, title: "Еще одна задача", isDone: true },
+    { id: 6, title: "Обучить", isDone: false },
+    { id: 7, title: "Повторить", isDone: true },
+  ];
+
+  // const liItems = [
+  //   <li>
+  //     <div>{tasks[0].title}</div>
+  //     <input type="checkbox" name="first" id="1" checked={tasks[0].isDone} />
+  //   </li>,
+  //   <li>
+  //     <div>{tasks[1].title}</div>
+  //     <input type="checkbox" name="second" id="2" checked={tasks[1].isDone} />
+  //   </li>,
+  //   <li>
+  //     <div>{tasks[2].title}</div>
+  //     <input type="checkbox" name="third" id="3" checked={tasks[2].isDone} />
+  //   </li>,
+  // ];
+
+  // const liCorrectItems = tasks.map((task) => (
+  //   <li>
+  //     <div>{task.title}</div>
+  //     <input type="checkbox" checked={task.isDone} />
+  //   </li>
+  // ));
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    <div>
+      <h1>Trelly Tasks</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <div>{task.title}</div>
+            <input type="checkbox" defaultChecked={task.isDone} />
+            {/*тут может быть checked но тогда в консоли вылезит замечание о том что ставить checked нежелательно и лучше другой проп*/}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
